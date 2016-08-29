5cb75c1946197fb2a63fde99f64006294616ec39
Bryan Burr <b.burr@ubreakifix.com>
2016-08-29 12:10:32 -0400

    fix breaking change in 0.2.14

--- release/angular-ui-router.js
+++ release/angular-ui-router.js
@@ -1293,7 +1293,6 @@ function $UrlMatcherFactory() {
     any: { // does not encode/decode
       encode: angular.identity,
       decode: angular.identity,
-      is: angular.identity,
       equals: angular.equals,
       pattern: /.*/
     }
